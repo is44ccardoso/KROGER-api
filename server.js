c4d8b40431d5a0f1daea257eaf515d8d4ba4f687
Júlia Kröger <julia.kroger@gmail.com>
2020-03-28 10:27:25 -0300

    changes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API IS RUNNING BEUTYFULY'))
+app.get('/', (req, res) => res.send('API is running beautifully!'))
 
 app.get('/fullname', (req, res) => {
   const name = 'Isaac'
   const lastname = 'Cardoso'
 
-  res.send(name + lastname)
+  res.send(`${name} ${lastname}`)
 })
 
 app.listen(5000, () => console.log('App is running'))
